f0cb7e6f8450172a64778b2bb2edcfca53f36352
Thalles Gabriel Lima Lossurdo <thallesgabriel1307@gmail.com>
2020-09-10 21:23:02 -0300

    master

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelectorAll('.input');
+
+function handleFocus() {
+  let parent = this.parentNode.parentNode;
+  console.log(this, this.parentNode, parent);
+  parent.classList.add('focus');  
+} 
+
+function handleBlur() {
+    let parent = this.parentNode.parentNode;
+    if(this.value == "") 
+     parent.classList.remove('focus'); 
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', handleFocus);
+    input.addEventListener('blur', handleBlur);
+});
+
+
